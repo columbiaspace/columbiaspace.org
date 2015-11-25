3432eea825740141f3bf4ce3773137f3da395ec4
Jake Lee <louisoel@gmail.com>
2015-11-24 23:57:06 -0500

    polishes and mission
    Some animation and JS polishes
    Added jqueryUI for animation speed
    Added mission template HTML

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+	var navOffset = $('.navbar').height();
+	console.log(navOffset);
+
+	$('body').scrollspy({
+	   offset: navOffset
+	});
+
+	$('#aboutNav a').click(function(event) {
+		console.log("hello");
+	    event.preventDefault();
+	    $('html, body').animate({
+            scrollTop: $(this.hash).offset().top - navOffset
+        }, 600,'easeInOutQuart');
+	    window.location.hash = $(this).attr('href');
+	});
+
+	$('#nav-logo a').click(function(event) {
+		console.log("hello2");
+	    event.preventDefault();
+	    $('html, body').animate({
+            scrollTop: "0px"
+        }, 600,'easeInOutQuart');
+	    window.location.hash = $(this).attr('href');
+	});
+});
